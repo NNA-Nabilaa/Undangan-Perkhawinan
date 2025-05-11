9ebaac1df6a9798777362b97813c4d112c9bf2ae
NNA Design <nabilahatiqah12@gmail.com>
2025-05-11 13:33:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,27 @@ function countdown(targetDate, elementId) {
   const update = () => {
     const now = new Date().getTime();
     const distance = countDownDate - now;
+
+    if (distance < 0) {
+      document.getElementById(elementId).innerHTML = "Hari Bahagia Telah Tiba!";
+      return;
+    }
+
     const d = Math.floor(distance / (1000 * 60 * 60 * 24));
     const h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const s = Math.floor((distance % (1000 * 60)) / 1000);
 
     document.getElementById(elementId).innerHTML =
-      d + "h " + h + "j " + m + "m " + s + "s ";
-
-    if (distance < 0) {
-      document.getElementById(elementId).innerHTML = "Hari Bahagia Telah Tiba!";
-    }
+      `${d}h ${h}j ${m}m ${s}s`;
   };
 
   update();
   setInterval(update, 1000);
 }
 
-countdown("June 30, 2025 11:00:00", "countdown");
-countdown("June 30, 2025 11:00:00", "footer-countdown");
+countdown("May 31, 2025 11:00:00", "countdown");
+countdown("May 31, 2025 11:00:00", "footer-countdown");
 
 function flipToNext() {
   document.querySelector('.flipbook').scrollBy({
